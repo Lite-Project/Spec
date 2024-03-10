5a712a4831d278998e5ea07f543b2cb3e267414c
Lite-Project <111746605+Lite-Project@users.noreply.github.com>
2024-03-11 10:27:12 +1300

    Update Spec.ps1

--- Spec.ps1
+++ Spec.ps1
@@ -1,24 +1,59 @@
 ﻿#
-# Quick Spec
+# Quick Spec W.I.P
 #
 # Made by: https://github.com/Lite-Project
 #
-# Version v0.1
+# Version v0.2
 #
-# W.I.P
+# **NEW**
 #
+# Added in v0.2
+# Using SmBiosMemoryType Property to find Memory type 
+# Grabs GPU only gpu if both GPU and APU are present.
 
 $RT = @()
 $info = Get-ComputerInfo
-$R = (Get-CimInstance Win32_PhysicalMemory | Select-Object Manufacturer, Speed, MemoryType)
-$RSM = @{ #Ram Speed Map
-    'DDR3' = 800
-    'DDR4' = 2133
-    'DDR5' = 4801
+$R = (Get-CimInstance Win32_PhysicalMemory | Select-Object Manufacturer, Speed, SMBiosMemoryType)
+[array]$GPUN = Get-CimInstance Win32_VideoController | Select -ExpandProperty Name
+[array]$GPUD = Get-CimInstance Win32_VideoController | Select -ExpandProperty DriverVersion
+if ($GPUN.Count -eq 1) {
+    $GPUN += "BLANK"
 }
-foreach ($module in $R) { #Using Speed map to determine RAM type
-    foreach ($type in $RSM.GetEnumerator() | Sort-Object Value -Descending) {
-        if ($module.Speed -ge $type.Value) {
+if ($GPUD.Count -eq 1) {
+    $GPUD += "BLANK"
+}
+$RTP = @{
+    'Unknown' = 0
+    'Other' = 1
+    'DRAM' = 2
+    'Synchronous DRAM' = 3
+    'Cache DRAM' = 4
+    'EDO' = 5
+    'EDRAM' = 6
+    'VRAM' = 7
+    'SRAM' = 8
+    'RAM' = 9
+    'ROM' = 10
+    'Flash' = 11
+    'EEPROM' = 12
+    'FEPROM' = 13
+    'EPROM' = 14
+    'CDRAM' = 15
+    '3DRAM' = 16
+    'SDRAM' = 17
+    'SGRAM' = 18
+    'RDRAM' = 19
+    'DDR' = 20
+    'DDR2' = 21
+    'DDR2 FB-DIMM' = 22
+    'DDR3' = 23
+    'FBD2' = 25
+    'DDR4' = 26
+    'DDR5' = 34
+} # RAM Type Map
+foreach ($module in $R) { #Using Type map to determine RAM type
+    foreach ($type in $RTP.GetEnumerator() | Sort-Object Value -Descending) {
+        if ($module.SMBiosMemoryType -ge $type.Value) {
             $RType = $type.Key
             break
         }
@@ -34,11 +69,9 @@ RAM: $($info.CsPhyicallyInstalledMemory / 1MB) GB
     Type: $($RT[0])
     Speed: $(($R.Speed | Measure-Object -Minimum).Minimum) Mhz
 
-GPU: $(Get-CimInstance Win32_VideoController | Select -ExpandProperty Name)
-    Driver Version: $(Get-CimInstance Win32_VideoController | Select -ExpandProperty DriverVersion)
+GPU: $($GPUN[0])
+    Driver Version: $($GPUD[0])
 
 MOTHERBOARD: $($info.CsManufacturer)
     Model: $($info.CsModel)
-
-WARNING: MEMORY TYPE CAN BE WRONG
 "@
